5955d941b00c3fcb1d4b5a805239c19a65271817
Lautaro Mabras <mabraslautaroezequiel@gmail.com>
2023-09-08 21:52:57 -0300

    json

--- js/app.js
+++ js/app.js
@@ -1,268 +1,29 @@
-class Pelicula {
-    constructor() {
-        this.IMDB = prompt('Ingrese el ID de la película (Debe cumplir lo siguiente: Debe comenzar con exactamente 2 letras (mayúsculas o minúsculas), luego, deben seguir con exactamente 7 dígitos. No se permite ningún otro carácter en la cadena. La longitud total de la cadena debe ser de 9 caracteres (2 letras + 7 dígitos). Por ejemplo "tt1234567")');
-        this.titulo = prompt('Ingrese el título:');
-        this.director = prompt('Ingrese el director:');
-        this.añoDeEstreno = parseInt(prompt('Ingrese el año de estreno:'));
-        this.pais = prompt('Ingrese el país de producción (separe los países con comas):').split(',');
-        this.generos = prompt('Ingrese los géneros de la película (Géneros aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary, Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western):').split(',');
-        this.calificacion = parseFloat(prompt('Ingrese la calificación:'));
-
-        while (true) {
-            try {
-                this.validacionId(this.IMDB);
-                this.validacionTitulo(this.titulo);
-                this.validacionDirector(this.director);
-                this.validacionAñoDeEstreno(this.añoDeEstreno);
-                this.validacionPais(this.pais);
-                this.validacionGenero(this.generos);
-                this.validacionCalificacion(this.calificacion);
-
-                const fichaTecnica = this.fichaTecnica();
-                alert(fichaTecnica);
-                break;
-            } catch (error) {
-                alert(error.message);
-                this.IMDB = prompt('Ingrese el ID de la película (Debe cumplir lo siguiente: Debe comenzar con exactamente 2 letras (mayúsculas o minúsculas), luego, deben seguir con exactamente 7 dígitos. No se permite ningún otro carácter en la cadena. La longitud total de la cadena debe ser de 9 caracteres (2 letras + 7 dígitos). Por ejemplo "tt1234567")');
-                this.titulo = prompt('Ingrese el título:');
-                this.director = prompt('Ingrese el director:');
-                this.añoDeEstreno = parseInt(prompt('Ingrese el año de estreno:'));
-                this.pais = prompt('Ingrese el país de producción (separe los países con comas):').split(',');
-                this.generos = prompt('Ingrese los géneros de la película (Géneros aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary, Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western):').split(',');
-                this.calificacion = parseFloat(prompt('Ingrese la calificación:'));
-            }
-        }
-    }
-
-    validacionId(IMDB) {
-        let regex = /^[A-Za-z]{2}[0-9]{7}$/;
-        if (!regex.test(IMDB)) {
-            throw new Error('El ID de IMDB no cumple con el formato requerido.');
-        }
-    }
-
-    validacionTitulo(titulo) {
-        if (titulo.length === 0) {
-            throw new Error('El título no puede estar vacío.');
-        }
-        if (titulo.length > 100) {
-            throw new Error('El título no puede exceder los 100 caracteres.');
-        }
-    }
-
-    validacionDirector(director) {
-        if (director.length === 0) {
-            throw new Error('El director no puede estar vacío.');
-        }
-        if (director.length > 50) {
-            throw new Error('El director no puede exceder los 50 caracteres.');
-        }
-    }
-
-    validacionAñoDeEstreno(añoDeEstreno) {
-        if (isNaN(añoDeEstreno) || añoDeEstreno.toString().length !== 4) {
-            throw new Error('El año de estreno no es válido.');
-        }
-    }
-
-    validacionPais(pais) {
-        if (!Array.isArray(pais) || pais.length === 0) {
-            throw new Error('Debe ingresar al menos un país.');
-        }
-    }
-
-    get generosAceptado() {
-        return "Los géneros aceptados son: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary, Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western";
-    }
-
-    validacionGenero(generos) {
-        const generosAceptados = [
-            "Action", "Adult", "Adventure", "Animation", "Biography", "Comedy",
-            "Crime", "Documentary", "Drama", "Family", "Fantasy", "Film Noir",
-            "Game-Show", "History", "Horror", "Musical", "Music", "Mystery",
-            "News", "Reality-TV", "Romance", "Sci-Fi", "Short", "Sport",
-            "Talk-Show", "Thriller", "War", "Western"
-        ];
-
-        for (let genero of generos) {
-            let lowerGenero = genero.trim().toLowerCase();
-            if (!generosAceptados.some((aceptado) => aceptado.toLowerCase() === lowerGenero)) {
-                throw new Error(`El género "${genero}" no es válido. ${this.generosAceptados}`);
-            }
-        }
-    }
-
-    validacionCalificacion(calificacion) {
-        if (isNaN(calificacion) || calificacion < 1 || calificacion > 10) {
-            throw new Error(`La calificación "${calificacion}" no es válida.`);
-        }
-    }
-
-    fichaTecnica() {
-        return `Datos sobre la película:\nID: ${this.IMDB}\nTítulo: ${this.titulo}\nDirector: ${this.director}\nAño de estreno: ${this.añoDeEstreno}\nPaís de producción: ${this.pais.join(', ')}\nGénero de la película: ${this.generos.join(', ')}\nCalificación: ${this.calificacion}`;
+const cargarMas = (elementoBoton, contenedorCajas) => {
+    const botonCargar = document.querySelector(elementoBoton);
+    const cajas = Array.from(document.querySelectorAll(contenedorCajas));
+    const cajasPorMostrar = cajas.slice(0, -1);
+    cajasPorMostrar.forEach(caja => {
+        caja.style.display = 'inline-block';
+    });
+    if (cajasPorMostrar[1].style.display === 'inline-block') {
+        botonCargar.style.display = 'none';
     }
 }
+document.getElementById('load-more-1').addEventListener("click", () => {
+    cargarMas('#load-more-1', '.box-container-1 .box-1');
+})
+document.getElementById('load-more-2').addEventListener("click", () => {
+    cargarMas('#load-more-2', '.box-container-2 .box-2');
+});
 
-const nuevaPelicula = new Pelicula();
+document.getElementById('load-more-3').addEventListener("click", () => {
+    cargarMas('#load-more-3', '.box-container-3 .box-3');
+});
 
-const mostrarMenu = () => {
-    const opcion = parseInt(prompt(`Seleccione una opción:
-    1. Eliminar duplicados en un array
-    2. Separar pares e impares en un array de números
-    3. FizzBuzz
-    4. Contar caracteres de un texto
-    5. Recortar texto
-    6. Comprobar si un número es primo
-    7. Aplicar descuento
-    8. Calcular edad
-    9. Salir`));
+document.getElementById('load-more-4').addEventListener("click", () => {
+    cargarMas('#load-more-4', '.box-container-4 .box-4');
+});
 
-    switch (opcion) {
-        case 1:
-            eliminarDuplicados();
-            break;
-        case 2:
-            separarParesImpares();
-            break;
-        case 3:
-            fizzBuzz();
-            break;
-        case 4:
-            contarCaracteres();
-            break;
-        case 5:
-            recortarTexto();
-            break;
-        case 6:
-            esPrimo();
-            break;
-        case 7:
-            aplicarDescuento();
-            break;
-        case 8:
-            calcularEdad();
-            break;
-        case 9:
-            alert('Saliendo del programa.');
-            break;
-        default:
-            alert('Opción no válida. Por favor, seleccione una opción válida.');
-            mostrarMenu();
-    }
-};
-
-const eliminarDuplicados = () => {
-    const input = prompt('Ingrese un array de elementos separados por comas:');
-    const array = input.split(',').map(item => item.trim());
-    if (Array.isArray(array)) {
-        const uniqueArray = [...new Set(array)];
-        alert(`Array sin duplicados: [${uniqueArray}]`);
-    } else {
-        alert('Error: El valor ingresado no es un array válido.');
-    }
-    mostrarMenu();
-};
-
-const separarParesImpares = () => {
-    const input = prompt('Ingrese un array de números separados por comas:');
-    const array = input.split(',').map(item => Number(item.trim()));
-    if (Array.isArray(array) && array.every(num => typeof num === 'number')) {
-        const pares = array.filter(num => num % 2 === 0);
-        const impares = array.filter(num => num % 2 !== 0);
-        alert(`Pares: [${pares}], Impares: [${impares}]`);
-    } else {
-        alert('Error: El valor ingresado no es un array numérico válido.');
-    }
-    mostrarMenu();
-};
-
-const fizzBuzz = () => {
-    const numero = parseInt(prompt('Ingrese un número:'));
-    if (!isNaN(numero)) {
-        let resultados = [];
-        for (let i = 1; i <= numero; i++) {
-            let mensaje = "";
-            if (i % 3 === 0) {
-                mensaje += "Fizz";
-            }
-            if (i % 5 === 0) {
-                mensaje += "Buzz";
-            }
-            if (mensaje === "") {
-                mensaje = i;
-            }
-            resultados.push(mensaje);
-        }
-        alert(`Resultados: ${resultados.join(', ')}`);
-    } else {
-        alert('Error: El valor ingresado no es un número válido.');
-    }
-    mostrarMenu();
-};
-
-const contarCaracteres = () => {
-    const texto = prompt('Ingrese un texto:');
-    if (typeof texto === 'string') {
-        alert(`Número de caracteres: ${texto.length}`);
-    } else {
-        alert('Error: El valor ingresado no es una cadena de texto.');
-    }
-    mostrarMenu();
-};
-
-const recortarTexto = () => {
-    const texto = prompt('Ingrese un texto:');
-    const longitud = parseInt(prompt('Ingrese la longitud a la que desea recortar el texto:'));
-    if (typeof texto === 'string' && !isNaN(longitud)) {
-        const resultado = texto.slice(0, longitud);
-        alert(`Texto recortado: "${resultado}"`);
-    } else {
-        alert('Error: Los parámetros ingresados no son válidos.');
-    }
-    mostrarMenu();
-};
-
-const esPrimo = () => {
-    const numero = parseInt(prompt('Ingrese un número:'));
-    if (!isNaN(numero) && numero > 1) {
-        for (let i = 2; i < numero; i++) {
-            if (numero % i === 0) {
-                alert('No es primo');
-                return;
-            }
-        }
-        alert('Es primo');
-    } else {
-        alert('Error: El valor ingresado no es un número válido o es menor o igual a 1.');
-    }
-    mostrarMenu();
-};
-
-const aplicarDescuento = () => {
-    const monto = parseFloat(prompt('Ingrese el monto:'));
-    const descuento = parseFloat(prompt('Ingrese el descuento en porcentaje:'));
-    if (!isNaN(monto) && !isNaN(descuento) && monto >= 0 && descuento >= 0 && descuento <= 100) {
-        const descuentoAplicado = monto * (descuento / 100);
-        const montoConDescuento = monto - descuentoAplicado;
-        alert(`Monto con descuento: $${montoConDescuento.toFixed(2)}`);
-    } else {
-        alert('Error: Los parámetros ingresados no son válidos.');
-    }
-    mostrarMenu();
-};
-
-const calcularEdad = () => {
-    const fechaString = prompt('Ingrese una fecha en formato yyyy-mm-dd:');
-    const fecha = new Date(fechaString);
-    if (fecha instanceof Date && !isNaN(fecha)) {
-        const hoy = new Date();
-        const diferencia = hoy.getTime() - fecha.getTime();
-        const añosPasados = Math.floor(diferencia / (1000 * 60 * 60 * 24 * 365));
-        alert(`Edad: ${añosPasados} años`);
-    } else {
-        alert('Error: El valor ingresado no es una fecha válida.');
-    }
-    mostrarMenu();
-};
-
-mostrarMenu();
+document.getElementById('load-more-5').addEventListener("click", () => {
+    cargarMas('#load-more-5', '.box-container-5 .box-5');
+});
---
+++ js/storage.js
@@ -0,0 +1,26 @@
+document.getElementById("login-form_login").addEventListener("submit", () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    alert("Usuario y contraseña guardados en localStorage.");
+    document.getElementById("storedUsername").textContent = username;
+    document.getElementById("storedPassword").textContent = password;
+});
+
+document.getElementById("deleteButton").onclick = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+
+    document.getElementById("storedUsername").textContent = "";
+    document.getElementById("storedPassword").textContent = "";
+
+    alert("Datos borrados de localStorage.");
+};
+const storedUsername = localStorage.getItem("username");
+const storedPassword = localStorage.getItem("password");
+
+if (storedUsername.length > 0 && storedPassword.length > 0) {
+    document.getElementById("storedUsername").textContent = storedUsername;
+    document.getElementById("storedPassword").textContent = storedPassword;
+}
